bbc054d426abebc69d1bd6c29ba6102ee7293d29
Ts1po <gio.tsipuria@gmail.com>
2025-02-15 19:02:29 +0400

    adding first ios app begin

---
+++ IconApp/IconApp/IconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconAppApp.swift
+//  IconApp
+//
+//  Created by George Tsipuria on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct IconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
